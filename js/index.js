41a508c6d0e53e062f9a9ebb203bc48784604696
Alvard <allasargsyan1111@gmail.com>
2023-07-19 12:19:16 +0400

    editted index.html file and deleted all menu cards from static html and then added cards from JavaScript used es6 classes

--- js/index.js
+++ js/index.js
@@ -119,7 +119,7 @@ setClock(".timer", "2023-07-13");
     }
 });
 
-const modalTimerId = setTimeout(openModal, 600000);
+// const modalTimerId = setTimeout(openModal, 600000);
 
 function showModalByScroll(){
     if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
@@ -139,7 +139,7 @@ function openModal (){
     modal.classList.add("show");
     modal.classList.remove("hide"); 
    document.body.style.overflow = "hidden";
-   clearTimeout(modalTimerId);
+//    clearTimeout(modalTimerId);
 }
 
 function closeModal (){
@@ -148,4 +148,71 @@ function closeModal (){
     document.body.removeAttribute("style");
 }
 // modal logic end
+
+
+//used Class for menu cards => start
+class MenuCard{
+    constructor(img, alt, title, descr, price, parentSelector){
+        this.img = img;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.price = price;
+        this.parent = document.querySelector(parentSelector);
+        this.transfer = 27;  
+        this.changToUAH();  
+    }
+
+    changToUAH(){
+        this.price = +this.price * this.transfer;
+    };
+
+    render(){
+        const{img, alt, title, descr, price,parent} = this;
+        const element = document.createElement("div");
+        element.classList.add("menu__item");
+        element.innerHTML = `
+            <img src=${img} alt=${alt}>
+            <h3 class="menu__item-subtitle">${title}</h3>
+            <div class="menu__item-descr">${descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+               <div class="menu__item-cost">Цена:</div>
+               <div class="menu__item-total"><span>${price}</span> грн/день</div>
+            </div>
+        `;
+
+        parent.append(element);
+    }
+};
+
+new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    "Меню \"Фитнес\"",
+    "Меню \"Фитнес\" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+    8.5,
+    ".menu .container"
+).render();
+
+new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    "Меню \"Премиум\"",
+    " В меню \"Премиум\"  мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+    20.4,
+    ".menu .container"
+).render();
+
+new MenuCard(
+    "img/tabs/post.jpg",
+    "vegy",
+    "Меню \"Постное\"",
+    "Меню \"Постное\" -  - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+    16,
+    ".menu .container"
+).render();
+//used Class for menu cards => end
+
+
 });
