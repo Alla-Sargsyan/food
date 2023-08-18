32fcc148e667298db251363c2efacf62119aca19
Alvard <allasargsyan1111@gmail.com>
2023-08-18 17:09:07 +0400

    we added webpack to our project, and then we configured webpack with config file, then we maked our index.js file to components type, eddited package.json file and added webpack script

---
+++ src/js/components/modal.js
@@ -0,0 +1,54 @@
+function modalModule() {
+    const modalTrigger = document.querySelectorAll("[data-modal]");
+    const modal = document.querySelector(".modal");
+    const modalCloseBtn = document.querySelector("[data-close]");
+    let flag = false;
+
+
+    modalTrigger.forEach(btn => btn.addEventListener("click", openModal));
+
+    function closeModal() {
+        modal.classList.remove("show");
+        modal.classList.add("hide");
+        document.body.removeAttribute("style");
+    }
+
+    function openModal() {
+        flag === true;
+        modal.classList.add("show");
+        modal.classList.remove("hide");
+        document.body.style.overflow = "hidden";
+        //    clearTimeout(modalTimerId);
+    }
+
+
+
+    modal.addEventListener("click", (e) => {
+        if (e.target === modal || e.target.getAttribute("data-close") == "") {
+            closeModal()
+        }
+    });
+
+    document.addEventListener("keydown", (e) => {
+        if (e.code === "Escape" && modal.matches(".show")) {
+            closeModal()
+        }
+    });
+
+    // const modalTimerId = setTimeout(openModal, 600000);
+
+    function showModalByScroll() {
+        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModal();
+            window.removeEventListener("scroll", showModalByScroll);
+        }
+        // if (window.scrollY >= 2000 && flag === false){
+        //     openModal();
+        //     window.removeEventListener("scroll",showModalByScroll );
+        // }
+    }
+
+    window.addEventListener("scroll", showModalByScroll);
+}
+
+module.exports = modalModule;
