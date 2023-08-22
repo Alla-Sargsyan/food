fc82af390c6677cc54273469ac5d1e760b91805c
Alvard <allasargsyan1111@gmail.com>
2023-08-23 00:32:13 +0400

    on our project we added babel and we fixed bugs about export and import issue

--- src/js/components/forms.js
+++ src/js/components/forms.js
@@ -1,123 +1,119 @@
 // const spinner = require("./spinner");
 import spinner from "./spinner";
+import { openModal, closeModal } from "./modal";
 
 function formsModule() {
-    const forms = document.querySelectorAll("form");
+	const forms = document.querySelectorAll("form");
+	const messages = {
+		loading: spinner,
+		success: "Thank you ! We will contact with you !",
+		failure: "Sorry, but something went wrong !"
+	};
 
-    const messages = {
-        loading: spinner,
-        success: "Thank you ! We will contact with you !",
-        failure: "Sorry,but something went wrong !"
-    }
+	forms.forEach(form => bindPostData(form));
 
-    forms.forEach(form => bindPostData(form));
+	async function postData(url, data) {
+		const request = await fetch(url, {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json; charset=utf-8"
+			},
+			body: data
+		});
 
-    async function postData(url, data) {
-        const request = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json; charset =utf-8"
-            },
-            body: data
-        });
+		if (!request.ok) {
+			throw new Error();
+		}
 
-        if (!request.ok) {
-            throw new Error();
-        }
+		return await request.json();
+	}
 
-        return await request.json();
-    }
+	async function getData(url) {
+		const request = await fetch(url);
 
-    async function getData(url) {
-        const request = await fetch(url);
+		if (!request.ok) {
+			throw new Error();
+		}
 
-        if (!request.ok) {
-            throw new Error();
-        }
+		return await request.json();
+	}
 
-        return await request.json();
-    }
+	function bindPostData(form) {
+		form.addEventListener("submit", (e) => {
+			e.preventDefault();
 
-    function bindPostData(form) {
-        form.addEventListener("submit", (e) => {
-            e.preventDefault();
+			const { loading, success, failure } = messages;
 
-            const { loading, success, failure } = messages;
+			const loader = document.createElement("div");
+			loader.innerHTML = loading();
+			form.append(loader);
 
-            const loader = document.createElement("div");
-            loader.innerHTML = loading();
-            form.append(loader);
+			if (!navigator.onLine) {
+				messagesModal(failure + ": " + "Please check your internet connection, and try again!");
+				loader.remove();
+				form.reset();
+			}
 
-            if (!navigator.onLine) {
-                messagesModal(failure + ":" + "Please check your interet connection,and try again!");
-                loader.remove();
-                form.reset();
-            }
+			const empty = /^$/g;
+			const phone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+			let status = false;
 
-            const empty = /^$/g;
-            const phone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-            let status = false;
+			for (let i = 0; i < form.querySelectorAll("input").length; i++) {
+				if (empty.test(form[i].value) || !phone.test(form[1].value)) {
+					status = false;
+					messagesModal("Please fill all fields, and on phone field please fill mobile number format");
+					loader.remove();
+					form.reset();
+					break;
+				} else {
+					status = true;
+				}
+			}
 
-            for (let i = 0; i < form.querySelectorAll("input").length; i++) {
-                if (empty.test(form[i].value) || !phone.test(form[1].value)) {
-                    status = false;
-                    messagesModal("Please fill all fields, and on phone field please fill mobile number format");
-                    loader.remove();
-                    form.reset();
-                    break;
-                } else {
-                    status = true;
-                }
-            }
+			if (status) {
+				const formData = new FormData(form);
+				// const data = JSON.stringify(Object.fromEntries(formData.entries()))
+				axios.post("http://localhost:8888/requests", Object.fromEntries(formData))
+					.then(res => {
+						console.log(res);
+						messagesModal(success);
+					})
+					.catch(err => {
+						messagesModal(failure + ": " + err);
+					})
+					.finally(() => {
+						loader.remove();
+						form.reset();
+					});
+			} else {
+				console.log("status is false");
+			}
+		});
+	}
 
-            if (status) {
-                const formData = new FormData(form);
-                //    const data = JSON.stringify(Object.fromEntries(formData.entries()))
-                // postData("http://localhost:8888/requests", data)
+	function messagesModal(message) {
+		const prevModalDialog = document.querySelector(".modal__dialog");
+		prevModalDialog.classList.add("hide");
+		openModal(document.querySelector(".modal"));
 
-                axios.post("http://localhost:8888/requests", Object.fromEntries(formData))
-                    .then(res => {
-                        console.log(res);
-                        messagesModal(success);
-                    })
-                    .catch(err => {
-                        messagesModal(failure + ":" + err);
-                    })
-                    .finally(() => {
-                        loader.remove();
-                        form.reset();
-                    });
-            } else {
-                console.log("status is false");
-            }
+		const messageModal = document.createElement("div");
+		messageModal.classList.add("modal__dialog");
+		messageModal.innerHTML = `
+			<div class="modal__content">
+				<div class="modal__close" data-close> &times; </div>
+				<div class="modal__title">${message}</div>
+			</div>
+		`;
 
+		document.querySelector(".modal").append(messageModal);
 
-        });
-    }
-
-    function messagesModal(message) {
-        const prewModalDialog = document.querySelector(".modal__dialog");
-        prewModalDialog.classList.add("hide");
-        openModal();
-
-        const messageModal = document.createElement("div");
-        messageModal.classList.add("modal__dialog");
-        messageModal.innerHTML = `
-            <div class = "modal__content">
-                <div class = "modal__close" data-close> &times; </div>
-                <div class = "modal__title">${message}</div>
-            </div>
-            `;
-
-        document.querySelector(".modal").append(messageModal);
-
-        setTimeout(() => {
-            messageModal.remove();
-            prewModalDialog.classList.add("show");
-            prewModalDialog.classList.remove("hide");
-            closeModal();
-        }, 2000)
-    }
+		setTimeout(() => {
+			messageModal.remove();
+			prevModalDialog.classList.add("show");
+			prevModalDialog.classList.remove("hide");
+			closeModal(document.querySelector(".modal"));
+		}, 2000);
+	}
 }
 
 // module.exports = formsModule;
--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,55 +1,54 @@
+function closeModal(modal) {
+	modal.classList.remove("show");
+	modal.classList.add("hide");
+	document.body.removeAttribute("style");
+}
+
+function openModal(modal) {
+	modal.classList.add("show");
+	modal.classList.remove("hide");
+	document.body.style.overflow = "hidden";
+	// clearTimeout(modalTimerId);
+}
+
 function modalModule() {
-    const modalTrigger = document.querySelectorAll("[data-modal]");
-    const modal = document.querySelector(".modal");
-    const modalCloseBtn = document.querySelector("[data-close]");
-    let flag = false;
+	const modalTrigger = document.querySelectorAll("[data-modal]");
+	const modal = document.querySelector(".modal");
+
+	modalTrigger.forEach(btn => btn.addEventListener("click", () => openModal(modal)));
+	
+	closeModal(modal);
+	openModal(modal);
 
 
-    modalTrigger.forEach(btn => btn.addEventListener("click", openModal));
+	modal.addEventListener("click", (e) => {
+		if (e.target === modal || e.target.getAttribute("data-close") == "") {
+			closeModal(modal);
+		}
+	});
 
-    function closeModal() {
-        modal.classList.remove("show");
-        modal.classList.add("hide");
-        document.body.removeAttribute("style");
-    }
+	document.addEventListener("keydown", (e) => {
+		if (e.key === "Escape" && modal.matches(".show")) {
+			closeModal(modal);
+		}
+	});
 
-    function openModal() {
-        flag === true;
-        modal.classList.add("show");
-        modal.classList.remove("hide");
-        document.body.style.overflow = "hidden";
-        //    clearTimeout(modalTimerId);
-    }
+	// const modalTimerId = setTimeout(openModal, 600000);
 
+	function showModalByScroll() {
+		// if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+		// 	openModal(modal);
+		// 	window.removeEventListener("scroll", showModalByScroll);
+		// }
+		if (window.scrollY >= 2000) {
+			openModal(modal);
+			window.removeEventListener("scroll", showModalByScroll);
+		}
+	}
 
-
-    modal.addEventListener("click", (e) => {
-        if (e.target === modal || e.target.getAttribute("data-close") == "") {
-            closeModal()
-        }
-    });
-
-    document.addEventListener("keydown", (e) => {
-        if (e.code === "Escape" && modal.matches(".show")) {
-            closeModal()
-        }
-    });
-
-    // const modalTimerId = setTimeout(openModal, 600000);
-
-    function showModalByScroll() {
-        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
-            openModal();
-            window.removeEventListener("scroll", showModalByScroll);
-        }
-        // if (window.scrollY >= 2000 && flag === false){
-        //     openModal();
-        //     window.removeEventListener("scroll",showModalByScroll );
-        // }
-    }
-
-    window.addEventListener("scroll", showModalByScroll);
+	window.addEventListener("scroll", showModalByScroll);
 }
 
 // module.exports = modalModule;
+export { closeModal, openModal };
 export default modalModule;
